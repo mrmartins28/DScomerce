9fedfa1e1c7cab26aea5c4aea7b0b4c9bc29a7b6
Leandro Martins <leandro2828@gmail.com>
2025-12-23 19:30:33 -0300

    OrdemItem, muitos para muitos com classe de associação

--- src/main/java/com/devsuperior/dscomerce2/entities/Order.java
+++ src/main/java/com/devsuperior/dscomerce2/entities/Order.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscomerce2.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -11,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -31,6 +34,9 @@ public class Order {
 	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 	}
