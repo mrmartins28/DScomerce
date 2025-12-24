a05e3426e8cef0793793ba91cd9503b62491fa00
Leandro Martins <leandro2828@gmail.com>
2025-12-23 21:32:18 -0300

    Equals e Hashcode

--- src/main/java/com/devsuperior/dscomerce2/entities/OrderItem.java
+++ src/main/java/com/devsuperior/dscomerce2/entities/OrderItem.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscomerce2.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -55,6 +57,31 @@ public class OrderItem {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public OrderItemPK getId() {
+		return id;
+	}
+
+	public void setId(OrderItemPK id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
