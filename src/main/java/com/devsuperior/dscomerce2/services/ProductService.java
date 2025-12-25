951a985a5d74ffc2179e68dc93048dd8ac3d6037
Leandro Martins <leandro2828@gmail.com>
2025-12-25 19:23:23 -0300

    GET products paged

--- src/main/java/com/devsuperior/dscomerce2/services/ProductService.java
+++ src/main/java/com/devsuperior/dscomerce2/services/ProductService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dscomerce2.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,11 +21,19 @@ public class ProductService {
 	
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
-		
+
 		Optional<Product> result = repository.findById(id);
-		Product product = result.get();
-		
+		Product product = result.get();	
 		return new ProductDTO(product);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		
+		Page<Product> result =  repository.findAll(pageable);
+		
+		return result.map(x -> new ProductDTO(x));
+				
 	}
 	
 
