571a6af1a1f26e7c5bfb2cb81f95ba5fce7bf320
Leandro Martins <leandro2828@gmail.com>
2025-12-25 20:46:38 -0300

    POST insert new product

--- src/main/java/com/devsuperior/dscomerce2/services/ProductService.java
+++ src/main/java/com/devsuperior/dscomerce2/services/ProductService.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscomerce2.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,5 +35,15 @@ public class ProductService {
 				
 	}
 	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
 
 }
